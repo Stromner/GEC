8392ba0da7bccba726b2e1a4a30da4fd16f04765
David Strömner <davidstromner@gmail.com>
2021-05-14 21:30:01 +0200

    Empty folder structure

---
+++ src/main/java/com/github/stromner/gec/GameEmulatorCollecorApplication.java
@@ -0,0 +1,13 @@
+package com.github.stromner.gec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameEmulatorCollecorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameEmulatorCollecorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/stromner/gec/GameEmulatorCollecorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.stromner.gec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameEmulatorCollecorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
