766a57a18818f1d8053726bba462f1401326d3fa
David Strömner <davidstromner@gmail.com>
2021-05-18 20:59:58 +0200

    Read in and display game list to ConsolePanel

--- Impl/src/main/java/gec/ui/components/panels/ConsolePanel.java
+++ Impl/src/main/java/gec/ui/components/panels/ConsolePanel.java
@@ -1,17 +1,40 @@
 package gec.ui.components.panels;
 
 import gec.core.ConsoleEnum;
+import gec.core.FileHandler;
+import gec.ui.components.elements.MenuPanel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.List;
 
 @Component
 public class ConsolePanel extends JPanel {
+    @Autowired
+    ResourceLoader resourceLoader;
+    private Font defaultFontSize = new Font("Serif", Font.BOLD, 12);
+    private Font selectedFontSize = new Font("Serif", Font.BOLD, 18);
     private ConsoleEnum selectedConsole;
+    private String gameListPath;
+    private List<String> gameList;
 
     public void init(ConsoleEnum selectedConsole) {
+        // TODO Loading screen while we are initiating
         this.selectedConsole = selectedConsole;
+        gameListPath = selectedConsole.getConsoleName() + ".txt";
+
+        gameList = FileHandler.readLinesFromFile(gameListPath);
+        createPanel();
+
+    }
+
+    private void createPanel() {
+        MenuPanel menu = new MenuPanel();
+        menu.init(defaultFontSize, selectedFontSize, gameList, new GridLayout(0, 1));
+        this.add(menu);
         this.setBackground(Color.BLACK);
     }
 }
